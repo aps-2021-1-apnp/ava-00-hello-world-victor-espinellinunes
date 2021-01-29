68892be77807ef87725e5fe151ea66dd9edc84ba
victor-espinellinunes <61764233+victor-espinellinunes@users.noreply.github.com>
2021-01-28 22:20:14 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,3 +5,11 @@ function hello(q: string): void {
 const quem: string = 'World'
 
 hello(quem)
+
+function bye(q: string): void {
+  console.log('Bye', q)
+}
+
+const quem2: string = 'World'
+
+bye(quem2)
